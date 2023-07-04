8bd89577cc2362b4af556b18076424db7dcd7da8
rda <2055017970@qq.com>
2023-07-04 16:19:13 +0800

    add some comments

--- signals/raise_sig.c
+++ signals/raise_sig.c
@@ -2,7 +2,7 @@
  * 首先程序注册一个信号的处理函数，然后调用raise，给进程自身发送信号，查看效果
  * 再修改处理函数，处理函数还会给自身发送一个相同的信号（此时该进程还在执行，应
  * 该默认是阻塞该信号，下次调度到该进程的时候再触发处理函数），推测执行结果是反
- * 反复复执行处理函数
+ * 反复复执行处理函数，还可以通过设置SA_NODEFER的标志，探究该标志的行为
  * NOTE: 具体的进程调度对信号的处理有什么影响目前还不清楚
  * */
 #include <signal.h>
@@ -33,6 +33,7 @@ int main(int argc, char **argv)
     struct sigaction act;
     act.sa_handler = sigHandler;
     act.sa_flags = SA_NODEFER;
+    sigemptyset(&act.sa_mask);
     if (sigaction(SIGINT, &act, NULL) == SIG_ERR)
         errExit("sigaction");
     if (sigaction(SIGUSR1, &act, NULL) == SIG_ERR)
--- signals/sig_resethand.c
+++ signals/sig_resethand.c
@@ -1,6 +1,10 @@
 /*
  * 这是exercise 20.16的20-3，验证sigaction的
- * SA_RESETHAND以及SA_NODEFER标志的效果
+ * SA_RESETHAND标志的效果
+ * 过程：使用sigaction设置SIGINT的处理函数，并且sigaction的flag为SA_RESETHAND
+ * 使用sig_sender发送两次SIGINT，第一次调用处理函数，第二次则执行默认行为，即终止
+ * 程序
+ * 原理：设置了SA_RESETHAND，会在调用处理器函数之前将信号处置重置为默认值
  * */
 
 #include <signal.h>
@@ -17,12 +21,15 @@ handler(int sig)
 int
 main(int argc, char ** argv)
 {
+    (void)argc;
+
     printf("%s: PID is %ld\n", argv[0], (long)getpid());
 
     struct sigaction act;
 
     act.sa_handler = handler;
     act.sa_flags = SA_RESETHAND;
+    sigemptyset(&act.sa_mask);
     (void)sigaction(SIGINT, &act, NULL);
 
     for (int i = 0; ; ++i)
