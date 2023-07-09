e02e16259cff0d9cbe27a6b3af86fe35f5cbacaf
rda <2055017970@qq.com>
2023-07-09 20:09:47 +0800

    add multi_wait.c: wait a few children

---
+++ procexec/multi_wait.c
@@ -0,0 +1,52 @@
+/**/
+#include <sys/wait.h>
+#include <time.h>
+#include "curr_time.h"
+#include "tlsp_hdr.h"
+
+int 
+main(int argc, char **argv)
+{
+    int numDead;
+    pid_t childPid;
+
+    if (argc < 2 || !strcmp(argv[1], "--help"))
+        usageErr("%s sleep-time...\n", argv[0]);
+
+    setbuf(stdout, NULL);
+
+    for (int j = 1; j < argc; ++j)
+    {
+        switch (fork())
+        {
+        case -1:
+            errExit("fork");
+        case 0:
+            printf("[%s] child %d started with PID %ld, sleeping %s seconds\n",
+                   currTime("%T"), j, (long)getpid(), argv[j]);
+            sleep(getInt(argv[j], GN_NONNEG, "sleep-time"));
+            _exit(EXIT_SUCCESS);
+        default:
+            break;
+        }
+    }
+
+    numDead = 0;
+    for (;;)
+    {
+        childPid = wait(NULL);
+        if (childPid == -1)
+        {
+            if (errno == ECHILD)
+            {
+                printf("No more children - bye!\n");
+                exit(EXIT_SUCCESS);
+            }
+            else
+                errExit("wait");
+        }
+        ++numDead;
+        printf("[%s] wait() returned child PID %ld (numDead=%d)\n",
+               currTime("%T"), (long)childPid, numDead);
+    }
+}
