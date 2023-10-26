b45a0c77ae7065d0273f92cefed0a558dc1de33a
rda <2055017970@qq.com>
2023-10-26 17:09:26 +0800

    thread join self: will not return successfully but return a error code

--- threads/thread_join_self.c
+++ threads/thread_join_self.c
@@ -1,7 +1,7 @@
 /*
  * exercise 29-1：线程join自身
  * expect：预计是会永久阻塞
- * real: 直接返回
+ * real: 直接返回，返回一个错误码但是不会修改errno，ERROR [EDEADLK/EDEADLOCK Resource deadlock avoided] pthread_join
  * */
 
 #include <pthread.h>
@@ -9,6 +9,13 @@
 
 int main()
 {
-    pthread_join(pthread_self(), NULL);
+    int s;
+    if ((s = pthread_join(pthread_self(), NULL)))
+    {
+        perror("pthread_join");
+        printf("%s\n", strerror(s));
+        errExitEN(s, "pthread_join\n");
+    }
+    
     exit(EXIT_SUCCESS);
 }
