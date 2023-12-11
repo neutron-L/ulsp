d3c03ad703491abb0e6862a89d3183838c675bc6
rda <2055017970@qq.com>
2023-12-11 16:41:14 +0800

    processpoll: init

---
+++ procpool/processpool.hh
@@ -0,0 +1,21 @@
+#ifndef PROCESSPOOL_HH_
+#define PROCESSPOOL_HH_
+
+#include <stdio.h>
+#include <assert.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/epoll.h>
+#include <signal.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+
+
+#endif
